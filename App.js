2420541254363fe16af6156422312192cf720413
Bob Naydenov <bob.naydenov@hotmail.com>
2020-11-28 22:17:27 +0000

    Finalise basic Cyberpunk release date counter

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, ImageBackground } from 'react-native';
+import { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
+
+const win = Dimensions.get('window')
+const ratio = win.width / 1000
 
 export default function App() {
+  const getTimeRemaining = () => {
+    const releaseDate = DateTime.fromISO('2020-12-10T00:00')
+    const timeRemaining = releaseDate.diffNow(['days', 'hours', 'minutes', 'seconds'], {}).toObject()
+    const { days, hours, minutes, seconds } = timeRemaining
+    let hours24Format = `0${hours}`
+
+    if (hours > 9) {
+      hours24Format = hours
+    }
+
+    return { days, hours: hours24Format, minutes, seconds: Math.floor(seconds) }
+  }
+  const [timeRemaining, setTimeRemaining] = useState(getTimeRemaining())
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining(getTimeRemaining())
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground style={styles.background} source={require('./assets/background.jpg')}>
+        <StatusBar style="auto" />
+        <Text style={styles.time}>{`${timeRemaining.days}:${timeRemaining.hours}:${timeRemaining.minutes}:${timeRemaining.seconds}`}</Text>
+        <View style={styles.animation}>
+          <Image style={styles.image} source={require('./assets/animation.gif')} />
+        </View>
+      </ImageBackground>
     </View>
   );
 }
@@ -17,5 +49,34 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    height: '100%',
   },
+  time: {
+    fontSize: 54,
+    fontFamily: 'Roboto',
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#f4e60c',
+    backgroundColor: '#00b8ff',
+    opacity: 0.8,
+    width: '100%',
+    marginTop: 100,
+    marginBottom:'auto',
+  },
+  animation: {
+    borderStyle: 'solid',
+    borderTopColor: '#f4e60c',
+    borderWidth: 1,
+  },
+  image: {
+    height: 300 * ratio,
+    width: win.width
+  },
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    width: '100%',
+  }
 });
